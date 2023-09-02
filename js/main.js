841998d5e7d35e86861c60e01af284d03192cfe3
Konstantin <konmitin.work@gmail.com>
2023-09-02 10:11:27 +0300

    fix: отсутствие смены логотипа в navbar с light на dark

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const navbar = document.querySelector(".navbar");
+const navbarLogoSvg = document.querySelector(".navbar__logo-svg use");
 
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar_light");
+    navbarLogoSvg.href.baseVal = "img/sprite.svg#logo-dark";
   } else {
     navbar.classList.remove("navbar_light");
+    navbarLogoSvg.href.baseVal = "img/sprite.svg#logo-light";
   }
 });
